fddd112d7144fb1abc50bd00ce1a8883922b98d9
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-06-19 16:37:00 -0600

    entities

--- AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/repositories/AssetRepository.java
+++ AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/repositories/AssetRepository.java
@@ -2,6 +2,8 @@ package com.skilldistillery.arewethereyet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.skilldistillery.retirementapp.entities.Asset;
+
 public interface AssetRepository extends JpaRepository<Asset, Integer> {
 
 }
