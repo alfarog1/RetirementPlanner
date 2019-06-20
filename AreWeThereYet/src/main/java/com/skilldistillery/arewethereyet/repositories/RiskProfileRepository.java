9b7fe2e379a7ffeb34ac0c7cf6133d948ff466f5
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-06-20 13:52:50 -0600

    app & properties

--- AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/repositories/RiskProfileRepository.java
+++ AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/repositories/RiskProfileRepository.java
@@ -1,9 +1,10 @@
 package com.skilldistillery.arewethereyet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import com.skilldistillery.retirementapp.entities.RiskProfile;
-
+@Component
 public interface RiskProfileRepository extends JpaRepository<RiskProfile, Integer> {
 
 }
