c8406e0bff3c0fa4c177f1d3153d33f34b021b37
Michael Quesada <michaelquesada@SDMacBookPro3-2.local>
2019-06-20 11:44:59 -0600

    Created auth service

--- ngAreWeThereYet/src/app/app.module.ts
+++ ngAreWeThereYet/src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { AreWeThereYetService } from './services/are-we-there-yet.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ChartsModule } from 'ng2-charts';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { AuthenticationService } from './services/authentication.service';
+import { AssetService } from "./services/asset.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ChartsModule } from "ng2-charts";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RiskQuestionnaireComponent } from './components/risk-questionnaire/risk-questionnaire.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HomeComponent } from "./components/home/home.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { RiskQuestionnaireComponent } from "./components/risk-questionnaire/risk-questionnaire.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { MyLineChartComponent } from "./my-line-chart/my-line-chart.component";
+import { EmployerMatchService } from "./services/employer-match.service";
+import { RiskProfileService } from "./services/risk-profile.service";
+import { UserProfileService } from "./services/user-profile.service";
+import { UserService } from "./services/user.service";
+import { VehicleService } from "./services/vehicle.service";
 
 @NgModule({
   declarations: [
@@ -34,10 +40,17 @@ import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
     ChartsModule,
     BrowserAnimationsModule,
     MatButtonModule,
-     MatCheckboxModule
-
+    MatCheckboxModule
   ],
-  providers: [AreWeThereYetService],
+  providers: [
+    AssetService,
+    EmployerMatchService,
+    RiskProfileService,
+    UserProfileService,
+    UserService,
+    VehicleService,
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
