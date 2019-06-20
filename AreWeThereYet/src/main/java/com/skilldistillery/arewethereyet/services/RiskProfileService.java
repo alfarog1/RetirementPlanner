16b22e810c6e4f3e24353b77d9c2efdbcc3707d6
Electrictoast <jkisor90@gmail.com>
2019-06-20 11:11:45 -0600

    basic crud for risk profiles

--- AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/services/RiskProfileService.java
+++ AreWeThereYet/src/main/java/com/skilldistillery/arewethereyet/services/RiskProfileService.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.arewethereyet.services;
 
+import java.util.List;
+
+import com.skilldistillery.retirementapp.entities.RiskProfile;
+
 public interface RiskProfileService {
+	List<RiskProfile> allRiskProfiles();
+	RiskProfile findById(int id);
+	RiskProfile create(RiskProfile risk);
+	RiskProfile replace(int id , RiskProfile risk);
+	Boolean deleteById(int id);
 
 }
